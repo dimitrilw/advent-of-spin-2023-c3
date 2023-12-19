19ed75de802b5b2b751ee870b0e6294355a932e5
Dimitri <5898931+dimitrilw@users.noreply.github.com>
2023-12-18 19:12:47 -0700

    initial framing of go-api
    Signed-off-by: Dimitri <5898931+dimitrilw@users.noreply.github.com>

---
+++ go-api/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+
+	spinhttp "github.com/fermyon/spin/sdk/go/v2/http"
+	// "github.com/fermyon/spin/sdk/go/v2/llm"
+)
+
+//go:embed styles.json
+var StyleOptionsJson string
+
+type StyleOptions struct {
+	Styles []string `json:"styles"`
+}
+func NewStyleOptions() (StyleOptions, error) {
+	var s StyleOptions
+	err := json.Unmarshal([]byte(StyleOptionsJson), &s)
+	if err != nil {
+		return StyleOptions{}, err
+	}
+	return s, nil
+}
+func (s StyleOptions) random() string {
+	rand.Seed(time.Now().Unix())
+	return s.Styles[rand.Intn(len(s.Styles))]
+}
+
+type Data struct {
+	Place      string   `json:"place"`
+	Characters []string `json:"characters"`
+	Objects    []string `json:"objects"`
+	Style      string   `json:"style,omitempty"`
+}
+func NewDataFromJsonRequest(r *http.Request) (Data, error) {
+	var d Data
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		return Data{}, err
+	}
+	if d.Style == "" {
+		s, err := NewStyleOptions()
+		if err != nil {
+			return Data{}, err
+		}
+		d.Style = s.random()
+	}
+	return d, nil
+}
+func (d Data) toPrompt() string {
+	return fmt.Sprintf(
+		`please generate a winter holiday story that:
+			references the Go programming language and/or Gophers;
+			and takes place at %s;
+			and contains these characters: a bunny, %s;
+			and references these objects: %s;
+			and is written in the style of %s;
+			and is limited to 500 words
+		`,
+		d.Place,
+		strings.Join(d.Characters, ", "),
+		strings.Join(d.Objects, ", "),
+		d.Style,
+	)
+}
+
+type Payload struct {
+	Story       string `json:"story"`
+	Style       string `json:"style"`
+	Nanoseconds int64  `json:"ns"`
+}
+
+func generateStoryFromPrompt(prompt string) (string, error) {
+	return prompt, nil
+}
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now().UnixNano()
+
+		d, err := NewDataFromJsonRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+
+		story, err := generateStoryFromPrompt(d.toPrompt())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		p := Payload{
+			Story:       story,
+			Style: 	     d.Style,
+			Nanoseconds: time.Now().UnixNano() - start,
+		}
+
+		json.NewEncoder(w).Encode(p)
+	})
+}
+
+func main() {}
